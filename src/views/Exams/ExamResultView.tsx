9167c438c66953be4163554ff9849bd2e830c184
kortkamp <marcelusmedius@gmail.com>
2022-06-24 08:37:11 -0300

    feat: use subject class selector

--- src/views/Exams/ExamResultView.tsx
+++ src/views/Exams/ExamResultView.tsx
@@ -1,13 +1,12 @@
 import { Card, CardContent, CardHeader, Grid, Box, TextField, MenuItem, Theme } from '@mui/material';
-import { DataGrid, GridColDef, GridColumnHeaderParams, GridOverlay, GridPagination } from '@mui/x-data-grid';
-import { ReactNode, useCallback, useEffect, useState } from 'react';
-import { useHistory } from 'react-router';
+import { DataGrid, GridColDef, GridColumnHeaderParams, GridOverlay } from '@mui/x-data-grid';
+import { useCallback, useEffect, useState } from 'react';
 import Moment from 'moment';
-import { AppButton } from '../../components';
-import { CommonDialog } from '../../components/dialogs';
+
 import { SHARED_CONTROL_PROPS } from '../../utils/form';
 import { examsService } from '../../services/exams.service';
 import makeStyles from '@mui/styles/makeStyles';
+import AppSubjectClassSelector from '../../components/AppSubjectClassSelector';
 
 interface IResult {
   value: number;
@@ -38,7 +37,6 @@ interface IExamResult {
   results: IResult[];
 }
 
-interface IDisplayResults {}
 interface IDisplayColumnResults {
   exam_id: string;
   title: string;
@@ -63,68 +61,72 @@ const useStyles = makeStyles((theme: Theme) => ({
  * url: /exames/notas
  */
 function ExamResultView() {
-  const [isSeaching, SetIsSearching] = useState(false);
+  const [subjectId, setSubjectId] = useState('');
+  const [classGroupId, setClassGroupId] = useState('');
 
   const [displayColumns, setDisplayColumns] = useState<IDisplayColumnResults[]>([]);
   const [displayRows, setDisplayRows] = useState<any[]>([]);
 
-  const [statusFilter, setStatusFilter] = useState('open');
-  const [typeFilter, setTypeFilter] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  const [examResults, setExamResults] = useState<IExamResult[]>([]);
-  const [loading, setLoading] = useState(true);
+  const loadExamResultsList = useCallback(() => {
+    let componentMounted = true;
 
-  const history = useHistory();
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const response = await examsService.getResultsByClassSubject(classGroupId, subjectId);
+        const exams = response.data.exams as IExamResult[];
 
-  const loadExamResultsList = useCallback(async () => {
-    setLoading(true);
+        const columns: IDisplayColumnResults[] = exams.map((exam) => ({
+          exam_id: exam.id,
+          title: exam.type,
+          date: exam.date,
+          value: exam.value,
+        }));
 
-    try {
-      const response = await examsService.getResultsByClassSubject(
-        'bb17930a-ae06-48ec-8102-80b602497ca1',
-        '52fe02ec-c902-4c89-922e-ec7a5c92b758'
-      );
-      const exams = response.data.exams as IExamResult[];
-      console.log(exams);
-      setExamResults(exams);
+        const rows = [] as any[];
 
-      const columns: IDisplayColumnResults[] = exams.map((exam) => ({
-        exam_id: exam.id,
-        title: exam.type,
-        date: exam.date,
-        value: exam.value,
-      }));
+        if (!componentMounted) {
+          return;
+        }
 
-      const rows = [] as any[];
-
-      exams.forEach((exam) => {
-        exam.results.forEach((result) => {
-          const rowIndex = rows.findIndex((row) => row.id === result.student.id);
-          if (rowIndex < 0) {
-            rows.push({ id: result.student.id, student: result.student.name, [exam.id]: result.value });
-          } else {
-            rows[rowIndex][exam.id] = result.value;
-          }
+        exams.forEach((exam) => {
+          exam.results.forEach((result) => {
+            const rowIndex = rows.findIndex((row) => row.id === result.student.id);
+            if (rowIndex < 0) {
+              rows.push({ id: result.student.id, student: result.student.name, [exam.id]: result.value });
+            } else {
+              rows[rowIndex][exam.id] = result.value;
+            }
+          });
         });
-      });
 
-      setDisplayRows(rows);
+        // setExamResults(exams);
 
-      setDisplayColumns(columns);
-    } catch (err: any) {
-      console.log(err);
-      // setError(ErrorAPI(404));
+        setDisplayRows(rows);
+
+        setDisplayColumns(columns);
+      } catch (err: any) {
+        console.log(err);
+      }
+      setLoading(false);
     }
-    setLoading(false);
-  }, [statusFilter]);
+
+    fetchData();
+
+    return () => {
+      componentMounted = false;
+    };
+  }, [classGroupId, subjectId]);
 
   useEffect(() => {
-    loadExamResultsList();
-  }, [loadExamResultsList, statusFilter]);
-
-  // useEffect(() => {
-  //   setFilteredExams(exams.filter((exam) => (typeFilter !== '' ? exam.type === typeFilter : true)));
-  // }, [typeFilter, exams]);
+    if (classGroupId && subjectId) {
+      loadExamResultsList();
+    } else {
+      setDisplayRows([]);
+    }
+  }, [loadExamResultsList, classGroupId, subjectId]);
 
   const columns: GridColDef[] = [
     {
@@ -158,9 +160,6 @@ function ExamResultView() {
 
   const classes = useStyles();
 
-  // if (Error) return Error as JSX.Element;
-  // if (loading) return <AppLoading />;
-
   return (
     <Grid container spacing={3}>
       <Grid item xs={12} md={12}>
@@ -168,30 +167,11 @@ function ExamResultView() {
           <CardHeader style={{ textAlign: 'center' }} title="Notas" subheader="Lista de notas" />
           <CardContent>
             <Grid container spacing={1}>
-              <Grid item xs={12} sm={12} md={3}>
-                <TextField
-                  select
-                  label="Turma"
-                  name="class_id"
-                  // value={statusFilter}
-                  // onChange={(e) => setStatusFilter(e.target.value)}
-                  {...SHARED_CONTROL_PROPS}
-                >
-                  <MenuItem value={'bb17930a-ae06-48ec-8102-80b602497ca1'}>teste</MenuItem>
-                </TextField>
-              </Grid>
-              <Grid item xs={12} sm={12} md={3}>
-                <TextField
-                  select
-                  label="Matéria"
-                  name="subject_id"
-                  // value={typeFilter}
-                  // onChange={(e) => setTypeFilter(e.target.value)}
-                  {...SHARED_CONTROL_PROPS}
-                >
-                  <MenuItem value={'52fe02ec-c902-4c89-922e-ec7a5c92b758'}>teste</MenuItem>
-                </TextField>
-              </Grid>
+              <AppSubjectClassSelector
+                onChange={(data) => {
+                  setSubjectId(data.subjectId), setClassGroupId(data.classGroupId);
+                }}
+              />
             </Grid>
 
             <DataGrid
@@ -203,7 +183,11 @@ function ExamResultView() {
               components={{
                 NoRowsOverlay: () => (
                   <GridOverlay>
-                    <div>Nenhuma avaliação encontrada</div>
+                    {subjectId && classGroupId ? (
+                      <div>Nenhuma avaliação encontrada</div>
+                    ) : (
+                      <div>Selecione Turma e Matéria</div>
+                    )}
                   </GridOverlay>
                 ),
               }}
