27e9934bcf61a97b9c91c0e92ce33fd2570624e9
kortkamp <marcelusmedius@gmail.com>
2022-06-27 22:47:31 -0300

    feat: add types to getAll exams

--- src/services/exams.service.ts
+++ src/services/exams.service.ts
@@ -1,11 +1,47 @@
 import api from './api.service';
 
-const getAll = async (per_page = 10, page = 1, filterBy = '', filterValue = '', filterType = '') =>
-  api.get(
-    `/exams?per_page=${per_page}&page=${page}&orderBy=created_at&orderType=DESC${
+interface IListExamResults {
+  id: string;
+  type: string;
+  status: string;
+  value: number;
+  weight: number;
+  teacher_id: string;
+  subject_id: string;
+  class_id: string;
+  date: Date;
+  created_at: string;
+  updated_at: string;
+  subject: {
+    id: string;
+    name: string;
+  };
+  class_group: {
+    id: string;
+    name: string;
+  };
+  teacher: {
+    id: string;
+    name: string;
+  };
+}
+
+interface IExamListResponse {
+  total_filtered: number;
+  page: number;
+  per_page: number;
+  total_pages: number;
+  result: IListExamResults[];
+}
+
+const getAll = async (per_page = 10, page = 1, filterBy = '', filterValue = '', filterType = '') => {
+  const response = await api.get(
+    `/exams?per_page=${per_page}&page=${page}&orderBy=date&orderType=ASC${
       filterValue ? `&filterBy=${filterBy}&filterValue=${filterValue}&filterType=${filterType}` : ''
     }`
   );
+  return response.data.exams as IExamListResponse;
+};
 const getResultsByClassSubject = async (class_id: string, subject_id: string) =>
   api.get(`/exams/subject/?class_id=${class_id}&subject_id=${subject_id}`);
 
