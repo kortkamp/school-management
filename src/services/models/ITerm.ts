f990fb10480d43a3e0abff8ad5d4d6ac3bf3e403
kortkamp <marcelusmedius@gmail.com>
2022-08-05 13:33:58 -0300

    feat: add term model

---
+++ src/services/models/ITerm.ts
@@ -0,0 +1,28 @@
+export enum TermType {
+  STANDARD = 'padrão',
+  EXTRA = 'extra',
+  RECOVERING = 'recuperação',
+  FINAL = 'recuperação final',
+}
+
+// TODO adicionar recuperacao bimestral, trimestral, semestral para calcularmos a notas de acordo com o tipo de recuperacao
+
+interface ITerm {
+  id: string;
+
+  name: string;
+
+  type: TermType;
+
+  start_at: Date;
+
+  end_at: Date;
+
+  school_id: string;
+
+  created_at: Date;
+
+  updated_at: Date;
+}
+
+export type { ITerm };
