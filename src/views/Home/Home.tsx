2d235c62f70d2516ea7ee5177db6fa7ae50f5e35
kortkamp <marcelusmedius@gmail.com>
2022-08-05 13:34:20 -0300

    feat: remove week table

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,37 +1,37 @@
 import { Card, CardContent, CardHeader, Grid } from '@mui/material';
 import OpenExams from '../../components/OpenExams';
-import WeekRoutines from '../../components/WeekRoutines';
-import { ITableCell } from '../../components/WeekRoutines/WeekRoutines';
-import { useAppStore } from '../../store';
+// import WeekRoutines from '../../components/WeekRoutines';
+// import { ITableCell } from '../../components/WeekRoutines/WeekRoutines';
+// import { useAppStore } from '../../store';
 
-const cellTable: ITableCell = ({ subject, classGroup }) => {
-  return (
-    <Grid container direction={'column'}>
-      <span>{classGroup}</span>
-      <span>{subject}</span>
-    </Grid>
-  );
-};
+// const cellTable: ITableCell = ({ subject, classGroup }) => {
+//   return (
+//     <Grid container direction={'column'}>
+//       <span>{classGroup}</span>
+//       <span>{subject}</span>
+//     </Grid>
+//   );
+// };
 
 /**
  * Renders "Home" view
  * url: / *
  */
 const HomeView = () => {
-  const [state] = useAppStore();
+  // const [state] = useAppStore();
 
-  const routinesTableType = state.currentSchool?.role as string;
+  // const routinesTableType = state.currentSchool?.role as string;
 
   return (
     <Grid container spacing={2}>
-      <Grid item xs={12} sm={12} md={8}>
+      {/* <Grid item xs={12} sm={12} md={8}>
         <Card>
           <CardHeader title="Minha semana" subheader="Horários da semana" />
           <CardContent>
             <WeekRoutines Cell={cellTable} type={routinesTableType} userId={state.currentUser?.id as string} />
           </CardContent>
         </Card>
-      </Grid>
+      </Grid> */}
       <Grid item xs={12} sm={12} md={4}>
         <Card>
           <CardHeader title="Avaliações" subheader="Próximas avaliações" />
