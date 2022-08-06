1ac20f9eec2a6d06c10a4d9194ec591465404193
kortkamp <marcelusmedius@gmail.com>
2022-08-06 14:56:52 -0300

    feat: add sortByDate

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,3 +1,7 @@
 export const sortByField = (source: any[], field: string) => {
   return source.sort((a, b) => (a[field] > b[field] ? 1 : -1));
 };
+
+export const sortByDate = (source: any[], field: string) => {
+  return source.sort((a, b) => b[field] - a[field]);
+};
