ff8bcd53ae6f7a8d888f6594acba9cabae1fb5d2
kortkamp <marcelusmedius@gmail.com>
2022-08-15 13:18:09 -0300

    feat: add loading to addButton

--- src/components/AppCustomButton/AppAddButton.tsx
+++ src/components/AppCustomButton/AppAddButton.tsx
@@ -2,11 +2,15 @@ import AppButton from '../AppButton';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import { ButtonProps } from '@mui/material';
 
+interface Props extends ButtonProps {
+  loading?: boolean;
+}
+
 /**
  * Application styled Material UI Save Button
  * @class AppAddButton
  */
-const AppAddButton: React.FC<ButtonProps> = ({ ...restOfProps }) => {
+const AppAddButton: React.FC<Props> = ({ ...restOfProps }) => {
   return (
     <AppButton color="info" startIcon={<AddCircleIcon />} {...{ ...restOfProps }}>
       Adicionar
