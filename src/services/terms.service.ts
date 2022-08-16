5de46e8dbffb438a602e020d014f8d9a905e9086
kortkamp <marcelusmedius@gmail.com>
2022-08-15 21:16:33 -0300

    feat: make termServices comply with useApi

--- src/services/terms.service.ts
+++ src/services/terms.service.ts
@@ -1,5 +1,6 @@
 import { IApiFuncParams } from '../api/useApi';
 import api from './api.service';
+import { ITerm } from './models/ITerm';
 
 export interface IListTerms {
   success: boolean;
@@ -12,16 +13,36 @@ export interface IListTerms {
   }[];
 }
 
+interface IDeleteTermProps extends IApiFuncParams {
+  args?: { id?: string };
+}
+
+interface IDeleteTermResponseData {
+  success: boolean;
+  message?: string;
+}
+
+interface ICreateTermResponseData {
+  success: boolean;
+  message?: string;
+  term?: ITerm;
+}
+
 const getAll = async ({ schoolId, token, cancelToken }: IApiFuncParams) =>
   (await api.get(`/${schoolId}/terms`, { headers: { Authorization: `Bearer ${token}` }, cancelToken }))
     .data as IListTerms;
 
 const create = async ({ schoolId, token, args }: IApiFuncParams) =>
-  api.post(`/${schoolId}/terms`, args.data, { headers: { Authorization: `Bearer ${token}` } });
+  (await api.post(`/${schoolId}/terms`, args.data, { headers: { Authorization: `Bearer ${token}` } }))
+    .data as ICreateTermResponseData;
 
-const remove = async (id: string) => api.delete('/terms/' + id);
+const remove = async ({ schoolId, token, args }: IDeleteTermProps) =>
+  (await api.delete(`/${schoolId}/terms/` + args?.id, { headers: { Authorization: `Bearer ${token}` } }))
+    .data as IDeleteTermResponseData;
 
-const update = async (id: string, data: object) => api.put('/terms/' + id, data);
+const update = async ({ schoolId, token, args }: IApiFuncParams) =>
+  (await api.put(`/${schoolId}/terms/${args.id}`, args.data, { headers: { Authorization: `Bearer ${token}` } }))
+    .data as ICreateTermResponseData;
 
 const getById = async ({ schoolId, token, args }: IApiFuncParams) =>
   (await api.get(`/${schoolId}/terms/${args.id}`, { headers: { Authorization: `Bearer ${token}` } })).data
