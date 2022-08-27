7582f5564f53187444ffcd760fd7b9d15a4b787a
kortkamp <marcelusmedius@gmail.com>
2022-08-27 15:51:03 -0300

    feat: add button label

--- src/components/AppCustomButton/AppSaveButton.tsx
+++ src/components/AppCustomButton/AppSaveButton.tsx
@@ -3,15 +3,16 @@ import SaveIcon from '@mui/icons-material/Save';
 import { ButtonProps } from '@mui/material';
 interface Props extends ButtonProps {
   loading?: boolean;
+  label?: string;
 }
 /**
  * Application styled Material UI Save Button
  * @class AppSaveButton
  */
-const AppSaveButton: React.FC<Props> = ({ ...restOfProps }) => {
+const AppSaveButton: React.FC<Props> = ({ label, ...restOfProps }) => {
   return (
     <AppButton color="success" startIcon={<SaveIcon />} {...{ ...restOfProps }}>
-      Salvar
+      {label || 'Salvar'}
     </AppButton>
   );
 };
