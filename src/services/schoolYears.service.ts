78a528ce6fb1a6e8fa88f81152839ee2ada978c2
kortkamp <marcelusmedius@gmail.com>
2022-09-04 10:15:24 -0300

    feat: add close school year service

--- src/services/schoolYears.service.ts
+++ src/services/schoolYears.service.ts
@@ -1,33 +1,58 @@
 import { IApiFuncParams } from '../api/useApi';
 import api from './api.service';
+import { TermType } from './models/ITerm';
+
+export interface ISchoolYear {
+  id: string;
+  name: string;
+  start_at: Date;
+  end_at: Date;
+  terms: {
+    id: string;
+    name: string;
+    start_at: Date;
+    end_at: Date;
+    type: TermType;
+  }[];
+}
 
 interface ISchoolYearResponseData {
   success: boolean;
-  schoolYear: {
-    id: string;
-    name: string;
-    school_id: string;
-    start_at: Date;
-    end_at: Date;
-  };
+  schoolYear: ISchoolYear;
 }
 
-const create = async ({ token, schoolId, args: data }: IApiFuncParams) => {
-  const response = await api.post(`/${schoolId}/school-years`, data, { headers: { Authorization: `Bearer ${token}` } });
+const create = async ({ token, schoolId, args }: IApiFuncParams) => {
+  const response = await api.post(`/${schoolId}/school-years`, args.data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   return response.data as ISchoolYearResponseData;
 };
 
-const update = async ({ token, schoolId, args: data }: IApiFuncParams) => {
-  const response = await api.put(`/${schoolId}/school-years/`, data, { headers: { Authorization: `Bearer ${token}` } });
+const update = async ({ token, schoolId, args }: IApiFuncParams) => {
+  const response = await api.put(`/${schoolId}/school-years/${args.id}`, args.data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
   return response.data as ISchoolYearResponseData;
 };
 
-const getById = async ({ token, schoolId }: IApiFuncParams) =>
-  (await api.get(`/${schoolId}/school-years`, { headers: { Authorization: `Bearer ${token}` } }))
+const getBySchool = async ({ token, schoolId }: IApiFuncParams) =>
+  (await api.get(`/${schoolId}/school-years/current`, { headers: { Authorization: `Bearer ${token}` } }))
     .data as ISchoolYearResponseData;
+
+const close = async ({ token, schoolId, args }: IApiFuncParams) => {
+  const response = await api.post(
+    `/${schoolId}/school-years/close/${args.id}`,
+    {},
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+  return response.data as ISchoolYearResponseData;
+};
 
 export const schoolYearsService = {
   create,
   update,
-  getById,
+  getBySchool,
+  close,
 };
