1653d29a3efb5b236a6f8ef72bbdccfeddbf11ed
kortkamp <marcelusmedius@gmail.com>
2022-09-12 13:42:37 -0300

    feat: add getall useApi complient

--- src/services/segments.service.ts
+++ src/services/segments.service.ts
@@ -1,11 +1,24 @@
+import { IApiFuncParams } from '../api/useApi';
 import api from './api.service';
 
-const getAll = async (per_page = 10, page = 1, filterBy = '', filterValue = '', filterType = '') =>
-  api.get(
-    `/segments?per_page=${per_page}&page=${page}&orderBy=name&orderType=DESC${
-      !!filterValue ? `&filterBy=${filterBy}&filterValue=${filterValue}&filterType=${filterType}` : ''
-    }`
-  );
+// const getAll = async (per_page = 10, page = 1, filterBy = '', filterValue = '', filterType = '') =>
+//   api.get(
+//     `/segments?per_page=${per_page}&page=${page}&orderBy=name&orderType=DESC${
+//       !!filterValue ? `&filterBy=${filterBy}&filterValue=${filterValue}&filterType=${filterType}` : ''
+//     }`
+//   );
+
+interface IListSegmentsResponse {
+  success: true;
+  segments: {
+    id: string;
+    name: string;
+  }[];
+}
+
+const getAll = async ({ schoolId, token, cancelToken }: IApiFuncParams) =>
+  (await api.get(`/${schoolId}/segments`, { headers: { Authorization: `Bearer ${token}` }, cancelToken })).data
+    .segments as IListSegmentsResponse['segments'];
 
 const create = async (data: object) => api.post('/segments', data);
 
