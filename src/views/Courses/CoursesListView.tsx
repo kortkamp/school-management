5f4af032ceae61de43f4f56237dc7d2be9916225
kortkamp <marcelusmedius@gmail.com>
2022-09-18 18:11:23 -0300

    feat: create courses list

--- src/views/Courses/CoursesListView.tsx
+++ src/views/Courses/CoursesListView.tsx
@@ -1,33 +1,75 @@
-import { Box, Card, CardContent, CardHeader, Divider, Paper, Tab, Tabs } from '@mui/material';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Box, Card, CardContent, CardHeader, Divider, Paper, Tab, Tabs, Theme } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import { useState } from 'react';
 import { AppAddButton } from '../../components/AppCustomButton';
-import CourseView from './CourseView';
+import CourseView, { FormValues } from './CourseView';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: { padding: '0px' },
+}));
 
 const CoursesListView = () => {
-  const [routineGroupsTabIndex, setRoutineGroupsTabIndex] = useState<number>(0);
+  const classes = useStyles();
+
+  const [coursesList, setCoursesList] = useState<Partial<FormValues>[]>([]);
+
+  const [courseIndex, setCourseIndex] = useState<number>(0);
+
+  const selectPreviousIndex = () => {
+    if (courseIndex > 0) {
+      setCourseIndex(courseIndex - 1);
+    }
+  };
 
   const handleChangeTab = (event: React.SyntheticEvent, index: number) => {
-    setRoutineGroupsTabIndex(index);
+    setCourseIndex(index);
+  };
+
+  const handleAddNewCourse = () => {
+    const newCoursesList = coursesList.concat([{ id: String(coursesList.length) }]);
+    setCoursesList(newCoursesList);
+    setCourseIndex(newCoursesList.length - 1);
+  };
+
+  const handleUpdateCourse = (data: FormValues) => {
+    const newCoursesList = coursesList.map((course) => {
+      if (course.id === data.id) {
+        return data;
+      }
+      return course;
+    });
+    setCoursesList(newCoursesList);
+  };
+
+  const handleRemoveCourse = (data: FormValues) => {
+    const newCoursesList = coursesList.filter((course) => course.id !== data.id);
+    setCoursesList(newCoursesList);
+    selectPreviousIndex();
   };
 
   return (
-    <Card style={{ padding: '20px' }}>
+    <Card className={classes.root}>
       <CardHeader style={{ textAlign: 'center' }} title="Cursos" subheader="Lista de cursos da instituição" />
-      <CardContent>
-        <Box component={Paper} sx={{ minWidth: 350 }}>
-          <Box display={'flex'} flexDirection="row" justifyContent="space-between">
-            <Tabs value={routineGroupsTabIndex} onChange={handleChangeTab}>
-              {[{ id: 1, name: 'teste' }].map((routineGroup) => (
-                <Tab key={routineGroup.id} label={routineGroup.name || 'Novo Curso'} />
-              ))}
-            </Tabs>
+      <CardContent component={Paper}>
+        <Box display={'flex'} flexDirection="row" justifyContent="space-between">
+          <Tabs value={courseIndex} onChange={handleChangeTab}>
+            {coursesList.map((routineGroup, index) => (
+              <Tab key={index} label={routineGroup?.name || 'Novo Curso'} />
+            ))}
+          </Tabs>
 
-            <AppAddButton style={{ marginRight: 6 }} color="info" label="Novo Curso" />
-          </Box>
-          <Divider />
-
-          <CourseView />
+          <AppAddButton style={{ marginRight: 6 }} color="info" label="Novo Curso" onClick={handleAddNewCourse} />
         </Box>
+        <Divider />
+        {coursesList.length > 0 && (
+          <CourseView
+            key={courseIndex + ' ' + coursesList.length}
+            data={{ ...coursesList[courseIndex] }}
+            onSave={handleUpdateCourse}
+            onRemove={handleRemoveCourse}
+          />
+        )}
       </CardContent>
     </Card>
   );
