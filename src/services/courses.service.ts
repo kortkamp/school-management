a9964222550c07d3d5ad9c6ccb433d285e8f1986
kortkamp <marcelusmedius@gmail.com>
2022-09-21 12:32:14 -0300

    feat: add courses services

---
+++ src/services/courses.service.ts
@@ -0,0 +1,44 @@
+import { IApiFuncParams } from '../api/useApi';
+import api from './api.service';
+
+export interface ICourse {
+  id: string;
+  name: string;
+  segment_id: string;
+  total_hours: number | '';
+  phase_name: string;
+  phases_number: number | '';
+  grades: { id: string; name: string; total_hours: number; days: number }[];
+}
+
+interface ICoursesResponseData {
+  success: boolean;
+  course: ICourse;
+}
+
+const create = async ({ token, schoolId, args: data }: IApiFuncParams) => {
+  const response = await api.post(`/${schoolId}/courses`, data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data as ICoursesResponseData;
+};
+
+const update = async ({ token, schoolId, args }: IApiFuncParams) => {
+  const response = await api.put(`/${schoolId}/courses/${args.id}`, args.data, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  return response.data as ICoursesResponseData;
+};
+
+const getAll = async ({ token, schoolId }: IApiFuncParams) =>
+  (await api.get(`/${schoolId}/courses/`, { headers: { Authorization: `Bearer ${token}` } })).data.courses as ICourse[];
+
+const remove = async ({ schoolId, token, args }: IApiFuncParams) =>
+  (await api.delete(`/${schoolId}/courses/` + args?.id, { headers: { Authorization: `Bearer ${token}` } })).data;
+
+export const coursesService = {
+  create,
+  update,
+  remove,
+  getAll,
+};
