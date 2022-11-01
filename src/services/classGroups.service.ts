0cf6d3f4ad5c2dc86a4481c1bd342d5e1ee51990
kortkamp <marcelusmedius@gmail.com>
2022-11-01 16:00:58 -0300

    feat: add classGroup service

--- src/services/classGroups.service.ts
+++ src/services/classGroups.service.ts
@@ -1,11 +1,38 @@
+import { IApiFuncParams } from '../api/useApi';
 import api from './api.service';
 
-const getAll = async (per_page = 10, page = 1, filterBy = '', filterValue = '', filterType = '') =>
-  api.get(
-    `/class-groups?per_page=${per_page}&page=${page}&orderBy=name&orderType=DESC${
-      !!filterValue ? `&filterBy=${filterBy}&filterValue=${filterValue}&filterType=${filterType}` : ''
-    }`
-  );
+interface IClassGroup {
+  id: string;
+  name: string;
+  grade: {
+    id: string;
+    name: string;
+    course: {
+      id: string;
+      name: string;
+    };
+  } | null;
+  routineGroup: {
+    id: string;
+    name: string;
+  } | null;
+}
+
+export interface IClassGroupDetail extends IClassGroup {
+  students: any[];
+  teachers: any[];
+}
+
+// const getAll = async (per_page = 10, page = 1, filterBy = '', filterValue = '', filterType = '') =>
+//   api.get(
+//     `/class-groups?per_page=${per_page}&page=${page}&orderBy=name&orderType=DESC${
+//       !!filterValue ? `&filterBy=${filterBy}&filterValue=${filterValue}&filterType=${filterType}` : ''
+//     }`
+//   );
+
+const getAll = async ({ schoolId, token, cancelToken }: IApiFuncParams) =>
+  (await api.get(`/${schoolId}/class-groups`, { headers: { Authorization: `Bearer ${token}` }, cancelToken })).data
+    .classGroups as IClassGroup[];
 
 const create = async (data: object) => api.post('/class-groups', data);
 
@@ -13,7 +40,15 @@ const remove = async (id: object) => api.delete('/class-groups/' + id);
 
 const update = async (id: string, data: object) => api.put('/class-groups/' + id, data);
 
-const getById = async (id: string) => api.get('/class-groups/' + id);
+// const getById = async (id: string) => api.get('/class-groups/' + id);
+
+const getById = async ({ schoolId, token, cancelToken, args }: IApiFuncParams) =>
+  (
+    await api.get(`/${schoolId}/class-groups/${args.id}`, {
+      headers: { Authorization: `Bearer ${token}` },
+      cancelToken,
+    })
+  ).data.classGroup as IClassGroupDetail;
 
 export const classGroupsService = {
   getAll,
