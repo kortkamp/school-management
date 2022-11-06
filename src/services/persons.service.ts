8cbd09e9ff00f3aa4eac31da5f1afcfa534b2d07
kortkamp <marcelusmedius@gmail.com>
2022-11-06 09:05:09 -0300

    feat: add id to interface

--- src/services/persons.service.ts
+++ src/services/persons.service.ts
@@ -9,9 +9,10 @@ export interface IGetPerson {
     cpf: string;
     rg: string;
     sex: string;
-    birth: string;
+    birth: Date;
     user: { id: string };
     addresses: {
+      id: string;
       street: string;
       number: string;
       complement: string;
@@ -30,7 +31,20 @@ const getByCPF = async ({ schoolId, token, args }: IApiFuncParams) =>
   (await api.get(`/${schoolId}/persons/cpf/${args.cpf}`, { headers: { Authorization: `Bearer ${token}` } }))
     .data as IGetPerson;
 
+const getById = async ({ schoolId, token, args }: IApiFuncParams) =>
+  (await api.get(`/${schoolId}/persons/${args.id}`, { headers: { Authorization: `Bearer ${token}` } })).data
+    .person as IGetPerson['person'];
+
+const update = async ({ schoolId, token, args }: IApiFuncParams) =>
+  (
+    await api.put(`/${schoolId}/persons/${args.id}`, args.data, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+  ).data;
+
 export const personsService = {
   create,
+  getById,
   getByCPF,
+  update,
 };
